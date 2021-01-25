62885fb4342aa7d107c95438b4a22f1c12b72218
ZMT <ZMT819@126.com>
2021-01-25 16:51:23 +0800

    修改progress配置

--- src/router/guard/progressGuard.ts
+++ src/router/guard/progressGuard.ts
@@ -1,7 +1,9 @@
 import type { Router } from 'vue-router'
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
-
+NProgress.configure({
+  showSpinner: false
+})
 export function createProgressGuard (router: Router) {
   router.beforeEach(() => {
     NProgress.start()
