a71802d9dfd06c83ad6e925754d7c625d700747c
ZMT <ZMT819@126.com>
2021-01-25 16:52:11 +0800

    types

--- src/components/types.ts
+++ src/components/types.ts
@@ -1,3 +1,30 @@
-import { defineComponent } from 'vue';
+import { defineComponent } from 'vue'
+import { Size } from '@/enums/components'
 
 export type Component = ReturnType<typeof defineComponent>;
+
+interface Formatter {
+  (row: any, column: any, cellValue: any, index: number): void
+}
+
+export interface TableColumn {
+  prop: string
+  label: string
+  width?: number
+  minWidth?: number
+  sortable?: boolean
+  popover?: boolean
+  formatter?: Formatter
+  showOverflowTooltip?: boolean,
+  customStyle?: boolean
+}
+
+export interface TableConfig {
+  height?: string
+  maxHeight?: string
+  stripe?: boolean
+  border?: boolean
+  size?: Size
+  highlightCurrentRow?: boolean,
+  selection?: boolean
+}
